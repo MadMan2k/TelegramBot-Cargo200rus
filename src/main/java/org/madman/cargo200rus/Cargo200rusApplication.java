7a4efed26825f49642fdd85b2f25e4843617bb7b
MadMan <mr.mad2k@gmail.com>
2022-08-20 20:51:48 +0300

    init commit

---
+++ src/main/java/org/madman/cargo200rus/Cargo200rusApplication.java
@@ -0,0 +1,13 @@
+package org.madman.cargo200rus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cargo200rusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cargo200rusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/madman/cargo200rus/Cargo200rusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.madman.cargo200rus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cargo200rusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
